0b45a197ffdb5d47641208d3df207f70b9044c87
DESKTOP-0HLORID\ghabr <ghabrielbmutti@gmail.com>
2021-09-30 23:18:48 -0300

    Creating tables

---
+++ src/databse/migrations/1633028906430-CreateUsers.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1633028906430 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varcher",
+          },
+          {
+            name: "email",
+            type: "varchar",
+          },
+          {
+            name: "admin",
+            type: "boolean",
+            default: false,
+          },
+          {
+            name: "created_at",
+            type: "timestpamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestpamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("user");
+  }
+}
