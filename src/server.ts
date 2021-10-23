5b8ffdb9d415c042d20446895b6d3f70fd86cb29
aLe-kLaus <alessomklaus13@gmail.com>
2021-10-23 19:41:34 -0300

    adding cors

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,10 @@ import { router } from "./routes";
 
 import "./database";
 const app = express();
+var cors = require("cors");
 
 app.use(express.json());
-
+app.use(cors());
 app.use(router);
 
 app.use(
